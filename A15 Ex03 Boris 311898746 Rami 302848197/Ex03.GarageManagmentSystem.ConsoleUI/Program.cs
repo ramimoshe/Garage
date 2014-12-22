188837e40ab8a224f9c9dfbea38316f7adb6ec83
ramimoshe <rms.rami.m@gmail.com>
2014-12-22 19:53:41 +0200

    First commit

---
+++ A15 Ex03 Boris 311898746 Rami 302848197/Ex03.GarageManagmentSystem.ConsoleUI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageManagmentSystem.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
