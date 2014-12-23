4d7d5e9f317930de2bfb2815a85497cec8b2fde0
unknown <rami.moshe@RamiM-PC.Conduit-IL.com>
2014-12-23 21:12:17 +0200

    add missing files

---
+++ A15 Ex03 BorisBorshevsky 311898746 RamiMoshe 302848197/Ex03.GarageLogic/Vehicles/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Ex03.GarageLogic.VehicleElements;
+
+namespace Ex03.GarageLogic.Vehicles
+{
+    public abstract class Vehicle
+    {
+        protected Vehicle(string i_ModelName, string i_SerialNumber, List<Wheel> i_Wheels)
+        {
+            ModelName = i_ModelName;
+            SerialNumber = i_SerialNumber;
+            Wheels = i_Wheels;
+        }
+
+        public string ModelName { get; private set; }
+
+        public string SerialNumber { get; private set; }
+
+        public abstract float GetEnergyLeftPrecent();
+
+        protected List<Wheel> Wheels;
+
+    }
+}
