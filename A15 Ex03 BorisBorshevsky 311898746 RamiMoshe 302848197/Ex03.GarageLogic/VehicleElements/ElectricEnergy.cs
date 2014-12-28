b77ed2679d7ee7189ce31d1400e1f67088329a72
boris <boris@firstofferz.com>
2014-12-28 21:16:37 +0200

    asd

--- A15 Ex03 BorisBorshevsky 311898746 RamiMoshe 302848197/Ex03.GarageLogic/VehicleElements/ElectricEnergy.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ex03.GarageLogic.VehicleElements
-{
-    public class ElectricEnergy : Energy
-    {
-        public ElectricEnergy(float i_Amount)
-            :base(i_Amount)
-        { }
-    }
-}
